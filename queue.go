17d805401ae99432523db9d3459098f31c0d84c6
Jan Paolo Dauz <jandauz@gmail.com>
2021-06-30 14:28:59 -0400

    Implement Queue.PeekByLookupID

--- queue.go
+++ queue.go
@@ -70,8 +70,6 @@ func (q *Queue) Peek(opts ...PeekOption) (Message, error) {
 }
 
 // PeekOption represents an option to peek messages in a queue.
-//
-// See: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms704311(v=vs.85)
 type PeekOption struct {
 	set func(opts *peekOptions)
 }
@@ -134,6 +132,102 @@ func PeekWithTimeout(timeout int) PeekOption {
 func PeekWithWantConnectorType(want bool) PeekOption {
 	return PeekOption{
 		set: func(opts *peekOptions) {
+			opts.wantConnectorType = want
+		},
+	}
+}
+
+// PeekByLookupID returns the message referenced by id but does not remove the
+// message from the queue.
+//
+// See: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms699797(v=vs.85)
+func (q *Queue) PeekByLookupID(id uint64, opts ...PeekByLookupIDOption) (Message, error) {
+	open, err := q.IsOpen()
+	if err != nil {
+		return Message{}, fmt.Errorf("go-msmq: failed to peek message by lookup id: %d: %w", id, err)
+	}
+
+	if !open {
+		return Message{}, fmt.Errorf("go-msmq: failed to peek message by lookup id: %d: %w", id, errors.New("Exception occurred. (The queue is not open or might not exist. )"))
+	}
+
+	options := &peekByLookupIDOptions{
+		wantDestinationQueue: false,
+		wantBody:             true,
+		wantConnectorType:    false,
+	}
+
+	for _, o := range opts {
+		o.set(options)
+	}
+
+	msg, err := q.dispatch.CallMethod(
+		"PeekByLookupId",
+		id,
+		options.wantDestinationQueue,
+		options.wantBody,
+		options.wantConnectorType,
+	)
+	if err != nil {
+		return Message{}, fmt.Errorf("go-msmq: PeekByLookupID(%d) failed to peek message by lookup id: %w", id, err)
+	}
+
+	return Message{
+		dispatch: msg.ToIDispatch(),
+	}, nil
+}
+
+// PeekOption represents an option to peek messages in a queue.
+//
+// See: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms704311(v=vs.85)
+type PeekByLookupIDOption struct {
+	set func(opts *peekByLookupIDOptions)
+}
+
+// peekOptions contains all the options to peek messages in a queue.
+type peekByLookupIDOptions struct {
+	wantDestinationQueue bool
+	wantBody             bool
+	wantConnectorType    bool
+}
+
+// PeekByLookupWithWantDestinationQueue returns a PeekOption that configures peeking
+// message with the specified want value.
+//
+// The default is false. If set to true, the Message.DestinationQueueInfo
+// property is updated when the message is read from the queue. Setting this
+// option to true may slow down the operation.
+func PeekByLookupWithWantDestinationQueue(want bool) PeekByLookupIDOption {
+	return PeekByLookupIDOption{
+		set: func(opts *peekByLookupIDOptions) {
+			opts.wantDestinationQueue = want
+		},
+	}
+}
+
+// PeekByLookupWithWantBody returns a PeekOption that configures peeking messages with
+// the specified want value.
+//
+// The default is true. It specifies that the body of the message should be
+// retrieved. If the message body is not needed, set this option to false to
+// optimize the speed of the application.
+func PeekByLookupWithWantBody(want bool) PeekByLookupIDOption {
+	return PeekByLookupIDOption{
+		set: func(opts *peekByLookupIDOptions) {
+			opts.wantBody = want
+		},
+	}
+}
+
+// PeekByLookupWithWantConnectorType returns a PeekOption that configures peeking
+// messages with the specified want value.
+//
+// The default is false. It specifies that MSMQ does not retrieve the
+// Message.ConnectorTypeGuid property when it peeks at a message in the
+// queue
+func PeekByLookupWithWantConnectorType(want bool) PeekByLookupIDOption {
+	return PeekByLookupIDOption{
+		set: func(opts *peekByLookupIDOptions) {
 			opts.wantConnectorType = want
 		},
 	}
